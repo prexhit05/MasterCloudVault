800d6fba019a8a3c36e90e2f92594998ae7727ec
prexhit05 <prexhit@gmail.com>
2025-11-18 18:22:42 +0530

    first commit

---
+++ src/main/java/com/masterCloudVault/MasterCloudVault/MasterCloudVaultApplication.java
@@ -0,0 +1,13 @@
+package com.masterCloudVault.MasterCloudVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterCloudVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterCloudVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masterCloudVault/MasterCloudVault/MasterCloudVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterCloudVault.MasterCloudVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterCloudVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
